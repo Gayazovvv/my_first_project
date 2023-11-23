f3d08d0d25088d4941d426207744ab6ebb123fd9
Bulat <bulatgaazov123@gmail.com>
2023-11-23 19:32:23 +0300

    Class

---
+++ Class.py
@@ -0,0 +1,73 @@
+from typing import Tuple
+from PIL import Image
+from math import exp, e
+
+class Filter:
+    def enhance(self, image, factor):
+        return image
+
+    def apply_to_pixel(self, r: int, g: int, b: int) -> Tuple[int, int, int]:
+        return r, g, b
+
+    def apply_to_image(self, path) -> Image.Image:
+        img = Image.open(path).convert("RGB")
+
+        for i in range(img.width):
+            for j in range(img.height):
+                r, g, b = img.getpixel((i, j))
+                new_colors = self.apply_to_pixel(r, g, b)
+                img.putpixel((i, j), new_colors)
+        return img
+
+class Exit(Filter):
+    def apply_to_image(self, path) -> Image.Image:
+        exit()
+
+# Блюр фильтр, играем с методом resize
+class BlurFilter(Filter):
+    def apply_to_image(self, path) -> Image.Image:
+        img = Image.open(path).convert("RGB")
+        width, height = img.size
+        factor = 4
+        small_image = img.resize((int(width / factor), int(height / factor)), Image.BILINEAR)
+        blurred_image = small_image.resize(img.size, Image.NEAREST)
+        return blurred_image
+
+# Перевод RGB картинки в чёрно-белую, через метод convert('L')
+class BlackWhite(Filter):
+    def apply_to_image(self, path) -> Image.Image:
+        image_file = Image.open(path).convert('L')
+        return image_file
+
+class Enhance(Filter):
+    def apply_to_image(self, path) -> Image.Image:
+        img = Image.open(path).convert("RGB")
+
+        # Применение затемнения к каждому пикселю
+        for i in range(img.width):
+            for j in range(img.height):
+                r, g, b = img.getpixel((i, j))
+
+                # Вычисление новых значений каждого канала RGB
+                new_r = int(r * 0.5)
+                new_g = int(g * 0.5)
+                new_b = int(b * 0.5)
+
+                # Применение новых значений к пикселю
+                img.putpixel((i, j), (new_r, new_g, new_b))
+
+        return img
+
+
+# ВЫХОД, ПРОВЕРИТЬ ПО ТЗ, ДОБАВИТЬ ПОСЛЕДНИЙ ФИЛЬТР В МЕНЮ
+
+
+
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,59 @@
+from PIL import Image
+from Class import Exit, BlurFilter, BlackWhite, Enhance
+import os
+
+
+def main():
+    filter_names = [
+        "Выход из приложения",
+        "Блюр",
+        "Конвертация изображения в чёрно-белый",
+        "Затемнение"
+
+    ]
+    filters = [
+        Exit(),
+        BlurFilter(),
+        BlackWhite(),
+        Enhance()
+    ]
+
+    print("Добро пожаловать в консольный фоторедактор.")
+
+
+    is_finished = False
+    while not is_finished:
+        path = input("Введите путь к файлу: ")
+
+        while not os.path.exists(path):
+            path = input("Файл не найден. Попробуй еще раз: ")
+
+
+        print("Какой фильтр вы хотите применить?")
+
+        for i in range(len(filter_names)):
+            print(f"{i} - {filter_names[i]}")
+
+        choice = input("Выберите фильтр (введите номер): ")
+
+        while not choice.isdigit() or int(choice) >= len(filters) or int(choice) < 0:
+            choice = input("Некорректный ввод. Попробуй еще раз: ")
+
+        filt = filters[int(choice)]
+        img = filt.apply_to_image(path)
+
+        save_path = input("Введите путь, куда сохранить файл: ")
+
+        img.save(save_path)
+        answer = input("Еще раз? (да/нет): ").lower()
+
+        while answer != "да" and answer != "нет":
+            answer = input("Некорректный ввод. Попробуй еще раз: ")
+        is_finished = answer == "нет"
+
+
+if __name__ == "__main__":
+    main()
+
+
+
